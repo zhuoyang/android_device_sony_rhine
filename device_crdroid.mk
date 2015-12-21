61001a09fa37f529f5db7ce4a1be47ff3f85c6cf
zhuoyang <zhuoyang98@gmail.com>
2015-12-21 10:59:52 +0800

    rhine. add Qcom perf stuff
    based on :
    https://github.com/crdroid-devices/android_device_sony_shinano/commit/e24d82c9f9d6d794c492ee309a2bee93dfb69aa4

--- device_crdroid.mk
+++ device_crdroid.mk
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ SONY_ROOT = device/sony/rhine/rootdir
+
+ PRODUCT_COPY_FILES += \
+     $(SONY_ROOT)/init.rhine.qc.rc:root/init.rhine.qc.rc
+
 # ANT+
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
@@ -24,6 +29,11 @@ PRODUCT_PACKAGES += \
 # Bootimg tools
 PRODUCT_PACKAGES += \
     extract_elf_ramdisk
+
+# QC Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.perf.cores_online=2 \
+    ro.vendor.extension_library=libqti-perfd-client.so
 
 # Recovery
 PRODUCT_PACKAGES += \
